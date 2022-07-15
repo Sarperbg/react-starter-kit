23cf5038ea5e0744b0abd9539d975312d0a46e03
sarperbg <sarpergulfirat@gmail.com>
2022-07-15 16:09:52 +0300

    second lesson completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      
+    };
+  }, [])
   return (
     <div className="App">
-     <img src='/logo192.png' alt=''/>
-     <img src={logo} alt=""/>
-     <h1>test</h1>
+     <h3>{process.env.NODE_ENV}</h3>
+    <p>
+      {process.env.REACT_APP_API_URL}
+    </p>
+    {process.env.NODE_ENV === 'production' && (
+      <>
+      <img src='"/logo192.png' alt='' />
+      <img src={logo} alt="" />
+      </>
+     )}
     </div>
   );
 }
