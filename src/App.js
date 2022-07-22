444cbd86856c52a29c434b3d2f7ce7abff3f208e
sarperbg <sarpergulfirat@gmail.com>
2022-07-22 23:01:31 +0300

    5.lesson completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './style.scss'
 import { createElement,Fragment } from "react"
-
-
-function Button(props) {
-  return <button>{props.text}</button>
-}   
+import "./tailwind.css" 
+import Button from "./components/Button"
+ 
 function App() {
 
+  const name = 'Sarper'
   const todos = ['todo1', 'todo2', 'todo3']
-  
+  /*
   const h1 = createElement('h1', null, 'prototurk.com')
   const ul = createElement('ul',null, todos.map(todo => createElement('li',null,todo)))
   const button = createElement(Button, {
@@ -17,14 +15,21 @@ function App() {
   return createElement('main', {
     className: 'test',
     id: 'main'
-  }, h1, ul,null )
-
+  }, h1, ul,button )
+ */
   const searchFunction = () => {
     alert("test")
   }
+ 
   return (
     <>
-    <Button text="merhaba" />
+    <div style={{padding:20}}>   
+    <Button text="buton örneği"/>
+    <Button text="buton örneği" variant="success"/>
+    <Button text="buton örneği" variant="danger"/>
+    <Button text="buton örneği" variant="warning"/>
+
+    </div>
       <h1 tabIndex="3" style= {{color:'red', backgroundColor:'yellow'}}>prototurk.com</h1>
       <label htmlFor="search" tabIndex="2" onClick={searchFunction}>Arama</label>
       <input type="text" id="search" tabIndex="1" />
